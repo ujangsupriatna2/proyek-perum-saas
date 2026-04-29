b172cef271acecbb8ce360b31addc8bae3da37c4
BRR Developer <dev@brr.co.id>
2026-04-29 15:47:47 +0000

    fix: use location_bg_image from settings for CTA section background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1414,7 +1414,7 @@ function CTASection() {
   return (
     <section className="relative py-32 md:py-40 overflow-hidden">
       <div className="absolute inset-0">
-        <img src={S.hero_bg_image || "/images/properties/hero_cover.png"} alt="" className="w-full h-full object-cover" />
+        <img src={S.location_bg_image || "/images/location.png"} alt="" className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-gray-950/85" />
         {/* Subtle grain */}
         <div className="absolute inset-0 opacity-[0.03]" style={{ backgroundImage: 'url("data:image/svg+xml,%3Csvg viewBox=\'0 0 256 256\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'noise\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'0.9\' numOctaves=\'4\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23noise)\'/%3E%3C/svg%3E")' }} />
